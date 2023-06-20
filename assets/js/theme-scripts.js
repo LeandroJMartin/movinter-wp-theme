14ead3f2ffe2841b03b8f7bd182b61a26e68cff6
leandroJMartin <leandro.dev@outlook.com.br>
2023-06-20 15:20:19 -0300

    Atualization files

--- assets/js/theme-scripts.js
+++ assets/js/theme-scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-
   /*-- Menu --*/
-  $('#open_menu').click(function() {
-      $('#menu').toggle('open');
-      $(this).toggleClass('on');
+  $("#open_menu").click(function () {
+    $("#menu").toggle("open");
+    $(this).toggleClass("on");
   });
 
-  $('#banner').owlCarousel({
+  $("#banner").owlCarousel({
     loop: false,
     margin: 1,
     mouseDrag: true,
@@ -16,19 +15,19 @@ $(document).ready(function () {
     responsiveClass: true,
     responsive: {
       0: {
-          items: 1,
+        items: 1,
       },
       768: {
-          items: 1,
+        items: 1,
       },
       1024: {
-          items: 1
-      }
-    }
+        items: 1,
+      },
+    },
   });
 
-  $('#expositores').owlCarousel({
-    loop: false,    
+  $("#expositores").owlCarousel({
+    loop: false,
     mouseDrag: true,
     autoplay: true,
     dots: true,
@@ -36,44 +35,70 @@ $(document).ready(function () {
     responsiveClass: true,
     responsive: {
       0: {
-          items: 2,
-          margin: 5
+        items: 2,
+        margin: 5,
       },
       768: {
-          items: 4,
-          margin: 15
+        items: 4,
+        margin: 15,
       },
       1024: {
-          items: 6,
-          margin: 25
-      }
+        items: 6,
+        margin: 25,
+      },
+    },
+  });
+
+  $(".open_logos").on("click", function () {
+    let obj = $(this).data("type");
+    $(`.${obj}`).css({ display: "block" });
+  });
+
+  function redirect_section() {
+    let position = $("#form").offset().top;
+
+    return $("html, body").animate({ scrollTop: position }, 600);
+  }
+  $(".btn-form").click(redirect_section);
+
+  $(".open_modal").on("click", function () {
+    let postname = $(this).attr("data-type");
+
+    $("#" + postname).css({ display: "block" });
+  });
+
+  $(".close-modal").click(function () {
+    $(".modal").css({ display: "none" });
+  });
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  // GET STATUS OF FORM HOME
+  (() => {
+    const form = document.querySelector("#form form");
+
+    if (form) {
+      const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(async function (mutation) {
+          if (mutation.attributeName === "data-status") {
+            const newDataValue = mutation.target?.dataset.status;
+
+            if (newDataValue === "sent") {
+              const hrefOrigin = window.location.origin;
+
+              await sleep(1000);
+
+              window.location.replace(`${hrefOrigin}/?form=success`);
+            }
+          }
+        });
+      });
+
+      observer.observe(form, {
+        attributes: true,
+      });
     }
-  });
+  })();
 
-  $('.open_logos').on('click', function(){
-
-    let obj = $(this).data('type');
-    $(`.${obj}`).css({'display':'block'});
-
-  });
-
-  function redirect_section(){
-    
-    let position = $('#form').offset().top;
-
-    return $("html, body").animate({scrollTop: position}, 600);
-
-  }$('.btn-form').click( redirect_section );
-
-
-  $('.open_modal').on('click', function(){
-    let postname = $(this).attr('data-type');
-
-    $('#' + postname).css({'display':'block'});
-  });
-
-  $('.close-modal').click(function(){
-    $('.modal').css({'display':'none'});
-  });
-
+  $(".wpcf7-select option:first-child").text("Eu sou");
 });
